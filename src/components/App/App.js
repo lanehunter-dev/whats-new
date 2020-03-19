8c1deaf6b68df33e3a7045e170856d1e05267b6b
Lane Hunter <lanehunter.96@gmail.com>
2020-03-18 19:00:22 -0600

    Add NewsContainer call to render method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NewsContainer from '../NewsContainer/NewsContainer';
 import local from '../../data/local';
 import './App.css';
 
@@ -6,15 +7,15 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      localNews: local,
     }
   }
 
   render () {
     return (
-      <div className="app">
-        YOUR CODE GOES HERE!
-      </div>
+      <main className="app">
+        <NewsContainer articles={this.state.localNews}/>
+      </main>
     );
   }
 }
