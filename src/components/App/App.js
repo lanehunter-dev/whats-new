5840f93997ca454be2aff6d3aa48c82d2ebd9f18
Lane Hunter <lanehunter.96@gmail.com>
2020-03-19 16:43:44 -0600

    Add changePage method and feed into menu as prop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 import NewsContainer from '../NewsContainer/NewsContainer';
+import Menu from '../Menu/Menu'
 import local from '../../data/local';
+import health from '../../data/health';
+import entertainment from '../../data/entertainment';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
+import '../Header/Header.css'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      localNews: local,
+      local: local,
+      health: health,
+      entertainment: entertainment,
+      science: science,
+      technology: technology,
+      currentPage: 'local'
     }
+  }
+
+  changePage = (pageName) => {
+    this.setState({currentPage: pageName})
   }
 
   render () {
     return (
-      <main className="app">
-        <NewsContainer articles={this.state.localNews}/>
-      </main>
+      <section className="app">
+        <header>
+          <h1>What's New?</h1>
+        </header>
+        <nav>
+          <Menu changePage={this.changePage}/>
+        </nav>
+        <main>
+          <NewsContainer articles={this.state[this.state.currentPage]}/>
+        </main>
+      </section>
     );
   }
 }
