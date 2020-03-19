d0af9a55c7591d5b904ce59e3416de0215b4ba54
Lane Hunter <lanehunter.96@gmail.com>
2020-03-19 16:48:29 -0600

    Add default JSX for new articles taking in props

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -4,7 +4,7 @@ import './NewsArticle.css';
 const NewsArticle = (props) => {
   return (
     <article id={props.id}>
-      <img src={props.img}></img>
+      <img src={props.img} alt={props.headline}></img>
       <h3>{props.headline}</h3>
       <p>{props.description}</p>
       <a href={props.url}>Link to article</a>
