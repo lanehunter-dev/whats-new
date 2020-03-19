25183e33141693ea6ca2e25d6d86c90b67807cdd
Lane Hunter <lanehunter.96@gmail.com>
2020-03-18 19:01:58 -0600

    Add props and map method to call NewsArticle

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import NewsArticle from '../NewsArticle/NewsArticle'
 import './NewsContainer.css'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = (props) => {
+  const newsArticles = props.articles.map(article => {
+    return (
+      <NewsArticle
+        id={article.id}
+        headline={article.headline}
+        img={article.img}
+        description={article.description}
+        url={article.url}
+        />
+    )
+  })
+  return (
+    <section className="news-container">
+      {newsArticles}
+    </section>
+  )
+}
 
 export default NewsContainer;
